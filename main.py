777db4543cbba167b6617a664cd15f63fd622fb0
olucasfracaro <lucasfracaro0403@gmail.com>
2025-05-24 22:38:36 -0300

    v2.5.5 melhorias na UX e UI

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from sys import exception
+from prettytable import PrettyTable
 import connsql, backend
-import os, hashlib, json as j
+import os, hashlib, json as j, sys
 from mysql.connector import ProgrammingError
 from datetime import datetime
 from getpass4 import getpass
+from termcolor import colored
 
 # Verifica o SO e define o comando de limpar a tela
 if os.name == 'nt': CL = "cls"
@@ -22,7 +23,30 @@ TAB = f"{mes}{ano}"
 
 COLUNAS_PADRAO = ["Etiqueta", "Educação", "Saúde", "Lazer", "Outros"]
 
+def adicionarGastos(con, cursor, conf):
+    """
+    Adicionar os gastos do dia simulado
+    """
+    colunasStr = ""
+
+    colunasValores = receberColunas(cols=conf[usuario]['colunas']) #pega os VALUES
+    colunasValores[0] = f"'{colunasValores[0]}'" # só coloca o '' na etiqueta pra não dar erro
+    subtotal = sum(colunasValores[1:]) #remove a coluna 'Etiqueta'
+
+    colunasValores = ", ".join(list(map(str, colunasValores))) #transforma tudo em string
+
+    colunasStr = ", ".join(conf[usuario]['colunas']) #pega o nome das colunas pra colocar no INTO
+
+    query = f"INSERT INTO {TAB} ({colunasStr}) VALUES ({dia}, {colunasValores}, {subtotal})"
+
+    cursor.execute(query)
+    connsql.mostrarTabela(cursor, "*", TAB)
+    con.commit()
+
 def configurarSenhaMestra(conf: dict):
+    """
+    Configurar a senha-mestra global a ser utilizada
+    """
     senhaMestra = getpass("Qual será a nova senha mestra? ").encode()
     with open('garracio.json', 'w') as f:
         conf['senhaMestra'] = hashlib.sha256(senhaMestra).hexdigest()
@@ -33,7 +57,7 @@ def configurarRMminmax(conf, usuario):
     Configura intervalo de dias (mínimo e máximo) para fazer o resumo mensal
     """
 
-    min, max = input("\nDigite o intervalo de tempo em dias para fazer o resumo mensal.\
+    min, max = input("\n⚙️ Digite o intervalo de tempo em dias para fazer o resumo mensal.\
                         \nFormato: min-max (incluindo os dois): ").split("-")
     
     conf[usuario]['config']['rmMinMax'] = [int(min), int(max)]
@@ -49,9 +73,9 @@ def resumoFeito(con, cursor, conf, usuario, semCheck=False):
                       int(dia) <= conf[usuario]['config']['rmMinMax'][1] )
                       and not conf[usuario]['config']['resumoMensalFeito']):
         
-        opc = input("Deseja resumir seu mês? (s/n) ")
+        opc = input("📝 Deseja resumir seu mês? (s/n) ")
         if opc.lower() == 's':
-            entra = float(input("Quanto você ganhou esse mês? R$"))
+            entra = float(input("💵 Quanto você ganhou esse mês? R$"))
             cursor.execute(connsql.criarTabela(mes, ano, res=True))
             _ = connsql.executar(cursor, f"SELECT SUBTOTAL FROM {TAB}")
             sai = 0 
@@ -69,33 +93,34 @@ def resumoFeito(con, cursor, conf, usuario, semCheck=False):
     return True
 
 
-def login(conf):
+def login(conf, usuario="arg"):
     """
     Lógica de login e criação de usuários para o Garracio
     """
 
     os.system(CL)
     
-    print(f"Usuários disponíveis: {", ".join(conf['databases'])}")
-    usuario = input("Login: ").capitalize()
+    if usuario == "arg":
+        print(f"🧑 Usuários disponíveis: {", ".join(conf['usuarios'])}")
+        usuario = input("🧑 Login: ").capitalize()
 
-    if usuario in conf['databases']:
+    if usuario in conf['usuarios']:
         connsql.config['database'] = usuario #verifica se o usuário existe
         while True: #checa a senha
-            _ = getpass("Senha-Mestra: ")
-            if _ == " ": print("Abortar!"); exit()
+            _ = getpass("🔒️ Senha-Mestra: ")
+            if _ == " ": print(colored("Abortar‼️", "red")); exit()
             if hashlib.sha256(_.encode()).hexdigest() == conf['senhaMestra']:
-                print("Login realizado!\n\n")
+                print("🔓️ Login realizado!\n\n")
                 break
             else:
-                print("Senha incorreta.")
+                print("❌ Senha incorreta.")
                 continue
 
     else: #criar usuário
-        _ = input("Usuário não encontrado!\nDeseja criá-lo (s/n)? ")
+        _ = input("🤔 Usuário não encontrado!\nDeseja criá-lo (s/n)? ")
 
         if _.lower() == 's':
-            conf['databases'].append(usuario)
+            conf['usuarios'].append(usuario)
             conf[usuario] = {
                 "config": {
                     "resumoMensalFeito": True,
@@ -124,20 +149,23 @@ def login(conf):
             configurarColunas(conf, usuario)
 
             os.system(CL)
-            print("Reinicie o programa para aplicar as alterações!")
+            print("🔄 Reinicie o programa para aplicar as alterações!")
             quit()
         elif _.lower() == 'n':
-            print("Abortar missão!")
+            print(colored("Abortar missão!", "red"))
             quit()
 
     con, cursor = connsql.conectar()
 
     return usuario, con, cursor
 
-def inicializar():
+def inicializar(usuario="arg"):
+    """
+    Inicializa as configurações, usuário e verifica se é dia de RM
+    """
     #carrega o arquivo de configuração na variável 'conf'
     with open("garracio.json", "r") as f: conf = j.load(f)
-    usuario, con, cursor = login(conf)
+    usuario, con, cursor = login(conf, usuario=usuario)
     conf[usuario]['config']['resumoMensalFeito'] = resumoFeito(con, cursor, conf, usuario)
     if dia == 1: conf[usuario]['config']['resumoMensalFeito'] = False
     with open("garracio.json", "w") as f: j.dump(conf, f, indent=4)
@@ -152,10 +180,10 @@ def receberColunas(cols=COLUNAS_PADRAO):
     
     for i in cols:
         if i == "Etiqueta":
-            _ = input(f"Etiqueta: ")
+            _ = input(f"\n✏️ Etiqueta: ")
             valores.append(_)
         elif i not in ["Dia", "SUBTOTAL"]:
-            _ = float(input(f"{i} R$"))
+            _ = float(input(f"💳️ {i} R$"))
             valores.append(_)
     return valores
 
@@ -165,10 +193,18 @@ def configurarColunas(conf, usuario):
     """
     cols = ["Dia", "Etiqueta"]
 
-    print("\nColunas padrão: ", ["Dia"] + COLUNAS_PADRAO + ["SUBTOTAL"])
-    print("Colunas atuais: ", conf[usuario]['colunas'])
+    colsPadrao = ["Dia"] + COLUNAS_PADRAO + ["SUBTOTAL"]
+    colsPadraoPT = PrettyTable() # o PT é PrettyTable tá pelo amor de Deus
+    colsPadraoPT.field_names = colsPadrao
 
-    cols_str = input("\nDigite as colunas em ordem, separadas por '; '\n(não é necessário incluir 'Dia', 'Etiqueta' e 'SUBTOTAL'): ")
+    colsAtuaisPT = PrettyTable()
+    colsAtuaisPT.field_names = conf[usuario]['colunas']
+
+    print(colored("\nColunas padrão: \n", "white", "on_grey"), colsPadraoPT)
+    print(colored("Colunas atuais: ", "black", "on_white"), colsAtuaisPT)
+
+    cols_str = input("\nDigite as colunas " + colored("em ordem, separadas por '; '", "red") +
+                     "\n(não é necessário incluir 'Dia', 'Etiqueta' e 'SUBTOTAL'): ")
 
     cols_usuario = cols_str.split("; ")
 
@@ -185,32 +221,31 @@ def configurarColunas(conf, usuario):
     return conf
  
 
-def main(pular_execucao=False):
+def main(tipoExecucao=0):
     """
     Função principal
     """
     global dia, mes, ano, usuario, con, cursor, conf
-    tipo_execucao = 1
 
     connsql.sincronizar(cursor)
 
-    if not pular_execucao:
-        tipo_execucao = int(input(f"{usuario}, selecione uma opção:\
-                                \n1-Executar diretamente (pela linha de comando)\
-                                \n2-Executar interface web (EXPERIMENTAL)\
+    if tipoExecucao == 0:
+        tipoExecucao = int(input(f"{usuario}, selecione a interface:\
+                                \n1-⌨️Executar diretamente (pela linha de comando)\
+                                \n2-🖥️Executar interface web (EXPERIMENTAL)\
                                 \n\n=>"))
     
-    if tipo_execucao == 1:
-        print(f"Data atual: {datetime.now().strftime('%d/%m/%Y')}")
-        print(f"Data simulada: {dia} de {mes}, {'20'+ano}\n")
-        _ = int(input(f"Olá {usuario}, bem vindo ao Garracio!\n\nO que deseja fazer hoje?\
-                    \n1-Adicionar gastos de hoje\
-                    \n2-Remover os gastos de um dia\
-                    \n3-Consultar um dia\
-                    \n4-Ver tabela do mês\
-                    \n5-Ver outra tabela\
-                    \n\n0-Opções\
-                    \n9-Sair\
+    if tipoExecucao == 1:
+        print(colored(f"📅 Data atual: {datetime.now().strftime('%d/%m/%Y')}", "white", "on_black"))
+        print(colored(f"📅 Data simulada: {dia} de {mes}, {'20'+ano}\n", "black", "on_blue"))
+        _ = int(input(colored(f"👋 Olá {usuario}", "yellow")+", bem vindo ao "+colored("Garracio", "black", "on_green")+"!\n\nO que deseja fazer hoje❓️\
+                    \n1-➕ Adicionar gastos de hoje\
+                    \n2-➖ Remover os gastos de um dia\
+                    \n3-🔎 Consultar um dia\
+                    \n4-📊 Ver tabela do mês\
+                    \n5-🧾 Ver outra tabela\
+                    \n\n0-⚙️ Opções\
+                    \n9-⬅️ Sair\
                     \n\n=>"))
 
         try: cursor.execute(connsql.criarTabela(mes, ano, colunas=conf[usuario]['colunas']))
@@ -218,53 +253,39 @@ def main(pular_execucao=False):
 
         match(_):
             case 1: # Adicionar gastos de hoje
-                colunas_str = ""
-
-                colunas_valores = receberColunas(cols=conf[usuario]['colunas']) #pega os VALUES
-                colunas_valores[0] = f"'{colunas_valores[0]}'" # só coloca o '' na etiqueta pra não dar erro
-                subtotal = sum(colunas_valores[1:]) #remove a coluna 'Etiqueta'
-
-                colunas_valores = ", ".join(list(map(str, colunas_valores))) #transforma tudo em string
-
-                colunas_str = ", ".join(conf[usuario]['colunas']) #pega o nome das colunas pra colocar no INTO
-
-                query = f"INSERT INTO {TAB} ({colunas_str}) VALUES ({dia}, {colunas_valores}, {subtotal})"
-
-                cursor.execute(query)
-                connsql.mostrarTabela(cursor, "*", TAB)
-                con.commit()
+                adicionarGastos(con, cursor, conf)
             case 2: # Remover gastos de um dia
                 connsql.mostrarTabela(cursor, "*", TAB)
-                id = input("Digite o ID da linha que você quer remover: ")
+                id = input("✏️ Digite o ID da linha que você quer remover: ")
                 cursor.execute(f"DELETE FROM {TAB} WHERE ID={id}")
                 con.commit()
             case 3: # Consultar dia
-                d = int(input("Qual dia você deseja ver? "))
+                d = int(input("📅 Qual dia você deseja ver? "))
                 connsql.executareMostrar(cursor, f"SELECT * FROM {TAB} WHERE Dia={d} ORDER BY Dia ASC")
             case 4: # Ver mês
                 connsql.mostrarTabela(cursor, "*", TAB)
             case 5: # Ver outra tabela
                 connsql.mostrarTabelas(cursor)
-                t = input("Digite o nome da tabela: ")
+                t = input("✏️ Digite o nome da tabela: ")
                 connsql.mostrarTabela(cursor, "*", t)
             case 9: # Sair
-                print(f"Tchau, {usuario}.\nNão se esqueça de mim!!")
+                print(f"\n👋 Tchau, {usuario}.\n"+colored("Não se esqueça de mim!!", "black", "on_red"))
                 quit()
             case 0: # Opções
                 os.system(CL)
-                _ = int(input(f"Selecione uma abaixo:\
-                            \n1-Alterar data\
-                            \n2-Realizar o resumo mensal\
-                            \n3-Configurar intervalo de dias para o resumo mensal\
-                            \n4-Mudar usuário\
-                            \n5-Alterar Senha-Mestra\
-                            \n6-Configurar colunas para {usuario}\
+                _ = int(input(f"⚙️ Selecione uma configuração:\n\
+                            \n1-📅 Alterar data\
+                            \n2-📆 Realizar o resumo mensal\
+                            \n3-⌛️ Configurar intervalo de dias para o resumo mensal\
+                            \n4-🧑 Mudar usuário\
+                            \n5-🔑 Alterar Senha-Mestra\
+                            \n6-📑 Configurar colunas para {usuario}\
                             \n\n=>"))
                 if _ == 1:
-                    dia = input("Dia: ")
-                    mes = connsql.numeropraMes(int(input("Mês: ")))
-                    ano = input("Ano: ")[2:4]
-                    main(pular_execucao=True)
+                    dia = input("📅 Dia: ")
+                    mes = connsql.numeropraMes(int(input("📅 Mês: ")))
+                    ano = input("📅 Ano: ")[2:4]
+                    main(tipoExecucao=1)
                 elif _ == 2:
                     resumoFeito(con, cursor, conf, usuario, semCheck=True)
                 elif _ == 3:
@@ -273,7 +294,7 @@ def main(pular_execucao=False):
                     with open("garracio.json", "r") as f: conf = j.load(f)
                     os.system(CL)
                     usuario, con, cursor = login(conf)
-                    main(pular_execucao=True)
+                    main(tipoExecucao=1)
                 elif _ == 5:
                     configurarSenhaMestra(conf)
                 elif _ == 6:
@@ -281,11 +302,15 @@ def main(pular_execucao=False):
                     configurarColunas(conf, usuario)
                     connsql.reconstruirTabela(cursor, conf, usuario, mes, ano)
                     
-    elif tipo_execucao == 2:
+    elif tipoExecucao == 2:
         backend.iniciar(host="brasa.onthewifi.com", usuario=usuario)
 
 
 if __name__ == "__main__":
-    conf, usuario, con, cursor = inicializar()
-    main()
-
+    conf, usuario, con, cursor = inicializar(usuario=sys.argv[-1]) if sys.argv[-1] not in connsql.EXCECOES else inicializar()
+    if "-web" in sys.argv:
+        main(tipoExecucao=2)
+    elif "-cli" in sys.argv:
+        main(tipoExecucao=1)
+    else:
+        main()
